a4b85d829649b4fec0cdc50b27729a58807bc29e
Alexon Conceição <alexonoffice@gmail.com>
2024-09-27 13:01:18 -0300

    calculadora de multas

---
+++ multa.js
@@ -0,0 +1,28 @@
+const args = process.argv.slice(2);
+
+const boletoIndex = args.indexOf('--boleto') + 1;
+const multaIndex = args.indexOf('--multa') + 1;
+const jurosDiaIndex = args.indexOf('--juros-dia') + 1;
+const diasIndex = args.indexOf('--dias') + 1;
+
+const boleto = boletoIndex > 0 ? parseFloat(args[boletoIndex]) : 0;
+const multa = multaIndex > 0 ? parseFloat(args[multaIndex]) : 0;
+const jurosDia = jurosDiaIndex > 0 ? parseFloat(args[jurosDiaIndex]) : 0;
+const dias = diasIndex > 0 ? parseFloat(args[diasIndex]) : 0;
+
+function showDados() {
+  console.log(`Boleto: $${boleto}`);
+  console.log(`Multa: ${multa}%`);
+  console.log(`Juros por Dia: ${jurosDia}%`);
+  console.log(`Dias: ${dias}`);
+}
+
+function calcularMulta() {
+  const valorMultaTotal =
+    (multa / 100) * boleto + (jurosDia / 100) * boleto * dias;
+  const valorFinal = boleto + valorMultaTotal;
+  console.log(`O valor total da multa é: $${valorFinal.toFixed(2)}`);
+}
+
+showDados();
+calcularMulta();
