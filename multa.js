bc01f6092453f411d8e2be6e249bd7aea7711389
Alexon Conceição <alexonoffice@gmail.com>
2024-09-28 15:13:10 -0300

    adicionado opção para funcionar com questões e argumentos também

--- multa.js
+++ multa.js
@@ -1,4 +1,44 @@
+const readline = require('readline');
 const args = process.argv.slice(2);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askQuestion(query) {
+  return new Promise((resolve) => {
+    rl.question(query, resolve);
+  });
+}
+
+async function askQuestions() {
+  const boleto = parseFloat(await askQuestion('Digite o valor do boleto: '));
+  const multa = parseFloat(await askQuestion('Digite o percentual da multa: '));
+  const jurosDia = parseFloat(
+    (await askQuestion('Digite o percentual de juros por dia (opcional): ')) ||
+      0
+  );
+  const jurosMes = parseFloat(
+    (await askQuestion('Digite o percentual de juros por mês (opcional): ')) ||
+      0
+  );
+  const dias = parseFloat(
+    (await askQuestion('Digite o número de dias para cálculo (opcional): ')) ||
+      0
+  );
+
+  if (isNaN(boleto) || isNaN(multa)) {
+    console.error('Erro: Boleto e multa são obrigatórios e devem ser números.');
+    process.exit(1);
+  }
+
+  showDados(boleto, multa, jurosDia, jurosMes, dias);
+  rl.question('Pressione qualquer tecla para encerrar...\n', () => {
+    rl.close();
+    process.exit(0);
+  });
+}
 
 const boletoIndex = args.indexOf('--boleto') + 1;
 const multaIndex = args.indexOf('--multa') + 1;
@@ -26,53 +66,53 @@ const jurosDia = jurosDiaIndex > 0 ? parseFloat(args[jurosDiaIndex]) : 0;
 const jurosMes = jurosMesIndex > 0 ? parseFloat(args[jurosMesIndex]) : 0;
 const dias = diasIndex > 0 ? parseFloat(args[diasIndex]) : 0;
 
-if (boleto === null || isNaN(boleto)) {
-  console.error(
-    'Erro: O valor do boleto é obrigatório e deve ser um número válido.'
-  );
-  process.exit(1);
+if (args.length === 0) {
+  askQuestions();
+} else {
+  if (boleto === null || isNaN(boleto)) {
+    console.error(
+      'Erro: O valor do boleto é obrigatório e deve ser um número válido.'
+    );
+    process.exit(1);
+  }
+
+  if (multa === null || isNaN(multa)) {
+    console.error(
+      'Erro: O percentual da multa é obrigatório e deve ser um número válido.'
+    );
+    process.exit(1);
+  }
+
+  showDados(boleto, multa, jurosDia, jurosMes, dias);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question('Pressione qualquer tecla para encerrar...\n', () => {
+    rl.close();
+    process.exit(0);
+  });
 }
 
-if (multa === null || isNaN(multa)) {
-  console.error(
-    'Erro: O percentual da multa é obrigatório e deve ser um número válido.'
-  );
-  process.exit(1);
-}
-
-function showDados() {
-  const valorFinal = calcularMulta();
+function showDados(boleto, multa, jurosDia, jurosMes, dias) {
+  const valorFinal = calcularMulta(boleto, multa, jurosDia, jurosMes, dias);
   console.log('\n======================================');
-  console.log(
-    `
+  console.log(`
 Dados:
 
 Boleto: $${boleto}
 Multa: ${multa}%${jurosDia !== 0 ? `\nJuros por Dia: ${jurosDia}%` : ''}${
-      jurosMes !== 0 ? `\nJuros por Mês: ${jurosMes}%` : ''
-    }
+    jurosMes !== 0 ? `\nJuros por Mês: ${jurosMes}%` : ''
+  }
 Dias em atraso: ${dias}
-O valor total a pagar é: $${valorFinal.toFixed(2)}`
-  );
+O valor total a pagar é: $${valorFinal.toFixed(2)}
+`);
   console.log('\n======================================');
 }
 
-function calcularMulta() {
+function calcularMulta(boleto, multa, jurosDia, jurosMes, dias) {
   const valorMultaTotal =
     (multa / 100) * boleto + (jurosDia / 100) * boleto * dias;
   return boleto + valorMultaTotal;
 }
-
-showDados();
-
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Pressione qualquer tecla para encerrar...\n', () => {
-  rl.close();
-  process.exit(0);
-});
